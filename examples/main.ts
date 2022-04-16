b187b16acb6283b82d9b227b0c4880683fd808f0
hayakawasho <hayakawa@pnrm.co.jp>
2022-04-17 00:51:38 +0900

    remove

--- examples/main.ts
+++
@@ -1,8 +0,0 @@
-import { defineComponent, onSetup, withSvelte } from '../dist/main.es.js'
-import Counter from './Counter.svelte'
-
-document.addEventListener('DOMContentLoaded', () => {
-  defineComponent('Counter', withSvelte(Counter))
-
-  onSetup()
-})
