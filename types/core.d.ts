bbeea75d2dd4956a62cf102f9d9395aeef04e5b7
hayakawasho <hayakawa@pnrm.co.jp>
2022-05-24 01:36:01 +0900

    build

--- types/core.d.ts
+++ types/core.d.ts
@@ -1,8 +1,13 @@
-import type { DOMNode, FC } from './internal/types';
-import type { ComponentContext } from './internal/component';
-export declare const defineComponent: (options: FC) => FC;
-export declare function register(name: string, componentWrapper: FC): Map<string, (el: DOMNode, props: Record<string, any>) => ComponentContext>;
-export declare function unregister(name: string): Map<string, (el: DOMNode, props: Record<string, any>) => ComponentContext>;
-export declare function mount(el: DOMNode, props: Record<string, any>, name: string): void;
-export declare function unmount(selector: string, scope?: HTMLElement): void;
+import type { ComponentContext } from './internal/component'
+import type { DOMNode, FC } from './internal/types'
+export declare const defineComponent: (options: FC) => FC
+export declare function register(
+  name: string,
+  componentWrapper: FC
+): Map<string, (el: DOMNode, props: Record<string, any>) => ComponentContext>
+export declare function unregister(
+  name: string
+): Map<string, (el: DOMNode, props: Record<string, any>) => ComponentContext>
+export declare function mount(el: DOMNode, props: Record<string, any>, name: string): void
+export declare function unmount(selector: string, scope?: DOMNode): void
 //# sourceMappingURL=core.d.ts.map
--- types/internal/types.d.ts
+++ types/internal/types.d.ts
@@ -1,18 +1,16 @@
-export declare type DOMNode = HTMLElement | SVGElement;
-export declare type Cleanup = () => (() => void) | void;
+export declare type DOMNode = HTMLElement | SVGElement
+export declare type Cleanup = () => (() => void) | void
 export interface FC {
-    setup(props: {
-        el: DOMNode;
-    } & Record<string, any>): Cleanup;
-    components?: {
-        [selector: string]: FC;
-    };
-    props?: Record<string, any>;
+  setup(el: DOMNode, props: Record<string, any>): Cleanup
+  components?: {
+    [selector: string]: FC
+  }
+  props?: Record<string, any>
 }
 export interface Context$ {
-    rootRef: DOMNode;
-    useDOMRef: <T>(...refKey: string[]) => {
-        refs: T;
-    };
+  rootRef: DOMNode
+  useDOMRef: <T>(...refKey: string[]) => {
+    refs: T
+  }
 }
 //# sourceMappingURL=types.d.ts.map
