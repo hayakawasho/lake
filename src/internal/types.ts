c41f19e86a333261978884c82bcb0158f62aaff9
hayakawasho <hayakawa@pnrm.co.jp>
2022-05-23 22:32:00 +0900

    update: merged  props

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -3,7 +3,11 @@ export type DOMNode = HTMLElement | SVGElement
 export type Cleanup = void | (() => unknown)
 
 export interface FC {
-  setup(el: DOMNode, props: Record<string, any>): Cleanup
+  setup(
+    props: {
+      el: DOMNode
+    } & Record<string, any>
+  ): Cleanup
   components?: {
     [selector: string]: FC
   }
