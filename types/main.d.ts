113462a4e963ab7a409fd8ebe757066812b87523
hayakawasho <hayakawa@pnrm.co.jp>
2022-06-01 03:21:44 +0900

    build

--- types/main.d.ts
+++ types/main.d.ts
@@ -3,4 +3,5 @@ export { q } from './util/selector'
 export { defineComponent, register, unregister, mount, unmount } from './core'
 export { withSvelte, getContext$ } from './svelte/withSvelte'
 export { useEvent } from './svelte/useEvent'
+export { withSolid } from './solid/withSolid'
 //# sourceMappingURL=main.d.ts.map
---
+++ types/solid/withSolid.d.ts
@@ -0,0 +1,5 @@
+import type { Component } from 'solid-js'
+export declare function withSolid(
+  App: Component<any>
+): import('../internal/types').IComponent<unknown>
+//# sourceMappingURL=withSolid.d.ts.map
