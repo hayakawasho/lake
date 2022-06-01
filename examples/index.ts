908e43c547549a2d177bef9c4894cb92f88d0f69
hayakawasho <hayakawa@pnrm.co.jp>
2022-06-02 01:51:22 +0900

    Update index.ts
    memo

--- examples/index.ts
+++ examples/index.ts
@@ -4,13 +4,10 @@ import Index from './IndexPage'
 document.addEventListener('DOMContentLoaded', () => {
   register('Index', Index)
 
-  // a simple mount component
+  // a simple how to mount component
   mount(document.body, {}, 'Index')
 
-  /**
-   * other how to mount component
-   */
-
+  // other example how to mount component
   // q('[data-component]').forEach(el => {
   //   const componentName = el.dataset.component as string
   //   mount(el, {}, componentName)
