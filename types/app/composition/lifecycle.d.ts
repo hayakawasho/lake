4e450dae2b0c28615d39bb70d08ba62b7ba2f437
hayakawasho <hayakawa@pnrm.co.jp>
2022-06-20 03:10:50 +0900

    build

--- types/app/composition/lifecycle.d.ts
+++ types/app/composition/lifecycle.d.ts
@@ -1,4 +1,8 @@
-import type { LifecycleHandler } from '../internal/component';
+export declare type LifecycleHandler = () => void;
+export declare const enum LifecycleHooks {
+  MOUNTED = 'onMounted',
+  UNMOUNTED = 'onUnmounted',
+}
 export declare const onMounted: (hook: LifecycleHandler) => void;
 export declare const onUnmounted: (hook: LifecycleHandler) => void;
 //# sourceMappingURL=lifecycle.d.ts.map
