be86c5656494b0f11800bca460d4ceffe41d3d5b
hayakawasho <hayakawa@pnrm.co.jp>
2023-01-18 12:25:49 +0900

    clean

--- types/core/composition/useIntersectionWatch.d.ts
+++
@@ -1,9 +0,0 @@
-import type { RefElement } from '../types';
-export declare const useIntersectionWatch: (
-  targetOrTargets: RefElement | RefElement[],
-  cb: IntersectionObserverCallback,
-  opts?: IntersectionObserverInit
-) => {
-  unwatch: (el: Element) => void;
-};
-//# sourceMappingURL=useIntersectionWatch.d.ts.map
